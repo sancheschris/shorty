750dcfad44d2d6dffacbf79e2c26937ccc07ce97
Christian Sanches <christian.sanches-v@uplevelteam.com>
2025-07-23 21:33:23 -0300

    fixing typo on database url

--- cmd/shorty/main.go
+++ cmd/shorty/main.go
@@ -1,6 +1,13 @@
 package main
 
+import (
+	"github.com/sancheschris/shorty/internal/config"
+	"github.com/sancheschris/shorty/internal/database"
+)
+
 func main() {
 
+	cfg := config.LoadConfig()
 
+	database.InitDB(cfg.DBUrl)
 }
