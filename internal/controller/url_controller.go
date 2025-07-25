3f819b3418b7c4d6fee87318088a9381eaef2304
Christian Sanches <christian.sanches-v@uplevelteam.com>
2025-07-24 21:31:16 -0300

    adds the url controller along with request/response

---
+++ internal/controller/url_controller.go
@@ -0,0 +1,34 @@
+package controller
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/sancheschris/shorty/internal/service"
+)
+
+type ShortenRequest struct {
+	OriginalURL string `json:"original_url" binding:"required"`
+}
+
+type ShortenResponse struct {
+	ShortURL string `json:"short_url"`
+}
+
+func ShortenURL(c *gin.Context) {
+	var req ShortenRequest
+
+	if err := c.ShouldBindJSON(&req); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid request payload"})
+		return
+	}
+
+	url, err := service.CreateShortURL(req.OriginalURL)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to create a short URL"})
+		return
+	}
+
+	shortURL := c.Request.Host + "/" + url.ShortCode
+	c.JSON(http.StatusOK, ShortenResponse{ShortURL: "http://" + shortURL})
+}
